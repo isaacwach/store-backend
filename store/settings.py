ae44191e3f51dd75473e8b36e4dd5708567709ee
felo <felix.kurgat@student.moringaschool.com>
2022-05-04 11:15:05 +0300

    aded changes

--- store/settings.py
+++ store/settings.py
@@ -14,7 +14,7 @@ from corsheaders.defaults import default_headers
 from corsheaders.signals import check_request_enabled
 from pathlib import Path
 import os
-import django_on_heroku
+import django_heroku
 import dj_database_url
 import cloudinary
 import cloudinary.uploader
@@ -231,4 +231,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_on_heroku.settings(locals())
+django_heroku.settings(locals())
